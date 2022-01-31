35835c4bdd7649c00f00b6ecf6fcdd058bf0ac57
mikey1384 <mikey1384@gmail.com>
2022-01-31 14:10:17 +0900

    theme color change

--- src/constants/css.js
+++ src/constants/css.js
@@ -1,8 +1,8 @@
 export const Color = {
-  blue: (opacity = 1) => `rgba(20,130,186,${opacity})`,
+  blue: (opacity = 1) => `rgba(95,115,148,${opacity})`,
   lightBlue: (opacity = 1) => `rgba(117,202,235,${opacity})`,
   darkBlue: (opacity = 1) => `rgba(0,70,195,${opacity})`,
-  logoBlue: (opacity = 1) => `rgba(75, 155, 225,${opacity})`,
+  logoBlue: (opacity = 1) => `rgba(95,115,148,${opacity})`,
   oceanBlue: (opacity = 1) => `rgba(33,150,243,${opacity})`,
   black: (opacity = 1) => `rgba(51,51,51,${opacity})`,
   brown: (opacity = 1) => `rgba(139,69,19,${opacity})`,
@@ -12,7 +12,7 @@ export const Color = {
   darkCyan: (opacity = 1) => `rgba(0,139,139,${opacity})`,
   gold: (opacity = 1) => `rgba(255,206,0,${opacity})`,
   logoGreen: (opacity = 1) => `rgba(97,226,101,${opacity})`,
-  green: (opacity = 1) => `rgba(40,182,44,${opacity})`,
+  green: (opacity = 1) => `rgba(189,157,137,${opacity})`,
   limeGreen: (opacity = 1) => `rgba(50, 205, 50,${opacity})`,
   yellowGreen: (opacity = 1) => `rgba(154,204,96,${opacity})`,
   lightCyan: (opacity = 1) => `rgba(224,255,255,${opacity})`,
@@ -31,9 +31,9 @@ export const Color = {
   whiteGray: (opacity = 1) => `rgba(250,250,250,${opacity})`,
   ivory: (opacity = 1) => `rgba(255,255,240,${opacity})`,
   brownOrange: (opacity = 1) => `rgba(245,190,70,${opacity})`,
-  orange: (opacity = 1) => `rgba(255,140,0,${opacity})`,
-  pink: (opacity = 1) => `rgba(255,105,180,${opacity})`,
-  purple: (opacity = 1) => `rgba(152,28,235,${opacity})`,
+  orange: (opacity = 1) => `rgba(112,98,97,${opacity})`,
+  pink: (opacity = 1) => `rgba(112,98,97,${opacity})`,
+  purple: (opacity = 1) => `rgba(171,164,175,${opacity})`,
   lightPurple: (opacity = 1) => `rgba(248,246,255,${opacity})`,
   red: (opacity = 1) => `rgba(255,65,54,${opacity})`,
   darkRed: (opacity = 1) => `rgba(139,0,0,${opacity})`,
@@ -42,7 +42,7 @@ export const Color = {
   rose: (opacity = 1) => `rgba(223,0,102,${opacity})`,
   vantaBlack: (opacity = 1) => `rgba(0,0,0,${opacity})`,
   white: (opacity = 1) => `rgba(255,255,255,${opacity})`,
-  yellow: (opacity = 1) => `rgba(253,253,150,${opacity})`
+  yellow: (opacity = 1) => `rgba(171,164,175,${opacity})`
 };
 
 export const borderRadius = '5px';
